027597cd05712206ba58822f04838dde78db63d1
honza214 <Jan.Fruhauf@student.souepl.cz>
2025-05-15 14:27:17 +0200

    Add files via upload

---
+++ list.py
@@ -0,0 +1,22 @@
+#pole = list(range(100))
+#print(pole)
+
+pole = []
+for i in range(10):
+    pole.append(i)
+print(pole)
+
+a = 3
+b = 8
+
+pom = [a,b]
+pole[a] = pole[b]
+pole[b] = pom
+print(print)
+
+
+
+
+
+
+
